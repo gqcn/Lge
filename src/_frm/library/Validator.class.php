0744f65b95bc316d4ce81287bf6ba8def33c036a
John <john@johng.cn>
2017-03-27 12:46:51 +0800

    文档更新，表单验证规则完善

--- src/_frm/library/Validator.class.php
+++ src/_frm/library/Validator.class.php
@@ -7,12 +7,57 @@ if (!defined('LGE')) {
 
 /**
  * 表单/数据校验类.
- * $rules = array(
- *     'true_name' => 'required',
- *     'user_pass' => array('required|length:6,24', array('required' => '用户名不能为空', 'length' => '用户名长度为6到24个字符')),
- *
- * );
- *
+
+    $data  = array(
+        'username'  => '',
+        'userpass'  => '1234567',
+        'userpass2' => '123456',
+    );
+    // 规则格式1
+    $rules = array(
+        'username'  => 'required',
+        'userpass'  => array('required', '用户密码不能为空'),
+        'userpass2' => array('required|same:userpass', array('请再次输入密码进行确认', '您两次输入的密码不一致')),
+    );
+
+    // 规则格式2
+    $rules = array(
+        'username'  => 'required',
+        'userpass'  => array('required', '用户密码不能为空'),
+        'userpass2' => array('required|same:userpass', array(
+            'required' => '请再次输入密码进行确认',
+            'same'     => '您两次输入的密码不一致')
+        ),
+    );
+
+    校验规则如下：
+    required           格式：required                      说明：必需参数
+    required_if        格式：required_if:field,value,...   说明：必需参数(当给定字段值与所给任意值相等时)
+    required_with      格式：required_with:foo,bar,...     说明：必需参数(当所给定任意字段值不为空时)
+    required_with_all  格式：required_with_all:foo,bar,... 说明：必须参数(当所给定所有字段值都不为空时)
+    date               格式：date                          说明：参数日期类型(使用strtotime进行判断)
+    date_format        格式：date_format:format            说明：判断日期是否为制定格式，format为PHP标准的日期格式
+    email              格式：email                         说明：邮件
+    phone              格式：phone                         说明：手机号
+    ip                 格式：ip                            说明：IP地址(IPv4|IPv6)
+    mac                格式：mac                           说明：MAC地址
+    url                格式：url                           说明：URL
+    length             格式：length:min,max                说明：参数长度为min到max
+    min_length         格式：min_length:min                说明：参数长度最小为min
+    max_length         格式：max_length:max                说明：参数长度最大为max
+    between            格式：between:min,max               说明：参数大小为min到max
+    min                格式：min:min                       说明：参数最小为min
+    max                格式：max:max                       说明：参数最大为max
+    json               格式：json                          说明：JSON
+    array              格式：array                         说明：数组
+    integer            格式：integer                       说明：整数
+    float              格式：float                         说明：浮点数
+    boolean            格式：boolean                       说明：布尔值(1,true,on,yes:true | 0,false,off,no,"":false)
+    same               格式：same:field                    说明：参数值必需与field参数的值相同
+    different          格式：different:field               说明：参数值不能与field参数的值相同
+    in                 格式：in:foo,bar,...                说明：参数值应该在foo,bar,...中
+    not_in             格式：not_in:foo,bar,...            说明：参数值不应该在foo,bar,...中
+
  * @author John
  */
 class Lib_Validator
@@ -23,20 +68,20 @@ class Lib_Validator
      */
     public static $defaultMessages = array(
         'required'          => '字段不能为空',
-        'required_if'       => '字段不能为空',                // required_if:field,value,...
-        'required_with'     => '字段不能为空',                // required_with:foo,bar,...
-        'required_with_all' => '字段不能为空',                // required_with_all:foo,bar,...
+        'required_if'       => '字段不能为空',
+        'required_with'     => '字段不能为空',
+        'required_with_all' => '字段不能为空',
         'date'              => '日期格式不正确',
-        'date_format'       => '日期格式不正确',              // date_format:format
+        'date_format'       => '日期格式不正确',
         'email'             => '邮箱地址格式不正确',
         'phone'             => '手机号码格式不正确',
         'ip'                => 'IP地址格式不正确',
         'mac'               => 'MAC地址格式不正确',
         'url'               => 'URL地址格式不正确',
-        'length'            => '字段长度为:min到:max个字符',   // length:min,max
+        'length'            => '字段长度为:min到:max个字符',
         'min_length'        => '字段最小长度为:min',
         'max_length'        => '字段最大长度为:max',
-        'between'           => '字段大小为:min到:max',        // between:min,max
+        'between'           => '字段大小为:min到:max',
         'min'               => '字段最小值为:min',
         'max'               => '字段最大值为:max',
         'json'              => '字段应当为JSON格式',
@@ -44,10 +89,10 @@ class Lib_Validator
         'integer'           => '字段应当为整数',
         'float'             => '字段应当为浮点数',
         'boolean'           => '字段应当为布尔值',
-        'same'              => '字段值不合法',                // same:field
-        'different'         => '字段值不合法',                // different:field
-        'in'                => '字段值不合法',                // in:foo,bar,...
-        'not_in'            => '字段值不合法',                // not_in:foo,bar,...
+        'same'              => '字段值不合法',
+        'different'         => '字段值不合法',
+        'in'                => '字段值不合法',
+        'not_in'            => '字段值不合法',
 
     );
 
@@ -91,14 +136,17 @@ class Lib_Validator
         if (is_array($rule)) {
             $ruleString = $rule[0];
             $messages   = $rule[1];
+            if (!is_array($messages)) {
+                $messages = array($messages);
+            }
         } else {
             $ruleString = $rule;
         }
         $ruleArray = explode('|', $ruleString);
-        foreach ($ruleArray as $ruleItem) {
+        foreach ($ruleArray as $ruleIndex => $ruleKey) {
             $ruleMatch   = true;
             $ruleMessage = '';
-            $tmpArray    = explode(':', $ruleItem);
+            $tmpArray    = explode(':', $ruleKey);
             $ruleName    = isset($tmpArray[0]) ? $tmpArray[0] : null;
             $ruleAttr    = isset($tmpArray[1]) ? $tmpArray[1] : null;
             switch ($ruleName) {
@@ -200,6 +248,8 @@ class Lib_Validator
                         $ruleMatch = false;
                         if (isset($messages[$ruleName])) {
                             $ruleMessage = $messages[$ruleName];
+                        } elseif (isset($messages[$ruleIndex])) {
+                            $ruleMessage = $messages[$ruleIndex];
                         } else {
                             $ruleMessage = self::$defaultMessages[$ruleName];
                             $ruleMessage = str_ireplace(array(':min', ':max'), array($min, $max), $ruleMessage);
@@ -215,6 +265,8 @@ class Lib_Validator
                         $ruleMatch = false;
                         if (isset($messages[$ruleName])) {
                             $ruleMessage = $messages[$ruleName];
+                        } elseif (isset($messages[$ruleIndex])) {
+                            $ruleMessage = $messages[$ruleIndex];
                         } else {
                             $ruleMessage = self::$defaultMessages[$ruleName];
                             $ruleMessage = str_ireplace(':min', $minLength, $ruleMessage);
@@ -230,6 +282,8 @@ class Lib_Validator
                         $ruleMatch = false;
                         if (isset($messages[$ruleName])) {
                             $ruleMessage = $messages[$ruleName];
+                        } elseif (isset($messages[$ruleIndex])) {
+                            $ruleMessage = $messages[$ruleIndex];
                         } else {
                             $ruleMessage = self::$defaultMessages[$ruleName];
                             $ruleMessage = str_ireplace(':max', $maxLength, $ruleMessage);
@@ -246,6 +300,8 @@ class Lib_Validator
                         $ruleMatch = false;
                         if (isset($messages[$ruleName])) {
                             $ruleMessage = $messages[$ruleName];
+                        } elseif (isset($messages[$ruleIndex])) {
+                            $ruleMessage = $messages[$ruleIndex];
                         } else {
                             $ruleMessage = self::$defaultMessages[$ruleName];
                             $ruleMessage = str_ireplace(array(':min', ':max'), array($min, $max), $ruleMessage);
@@ -260,6 +316,8 @@ class Lib_Validator
                         $ruleMatch = false;
                         if (isset($messages[$ruleName])) {
                             $ruleMessage = $messages[$ruleName];
+                        } elseif (isset($messages[$ruleIndex])) {
+                            $ruleMessage = $messages[$ruleIndex];
                         } else {
                             $ruleMessage = self::$defaultMessages[$ruleName];
                             $ruleMessage = str_ireplace(':min', $min, $ruleMessage);
@@ -274,6 +332,8 @@ class Lib_Validator
                         $ruleMatch = false;
                         if (isset($messages[$ruleName])) {
                             $ruleMessage = $messages[$ruleName];
+                        } elseif (isset($messages[$ruleIndex])) {
+                            $ruleMessage = $messages[$ruleIndex];
                         } else {
                             $ruleMessage = self::$defaultMessages[$ruleName];
                             $ruleMessage = str_ireplace(':max', $max, $ruleMessage);
@@ -304,7 +364,11 @@ class Lib_Validator
             if (!empty($ruleMessage)) {
                 $result[$ruleName] = $ruleMessage;
             } elseif (!$ruleMatch) {
-                $result[$ruleName] = isset($messages[$ruleName]) ? $messages[$ruleName] : self::$defaultMessages[$ruleName];
+                if (isset($messages[$ruleIndex])) {
+                    $result[$ruleName] = $messages[$ruleIndex];
+                } else {
+                    $result[$ruleName] = isset($messages[$ruleName]) ? $messages[$ruleName] : self::$defaultMessages[$ruleName];
+                }
             }
         }
         return $result;
