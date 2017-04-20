55b66fc5053cf0f1d1070237ed1c907e63c3af26
John <john@johng.cn>
2017-04-20 14:51:01 +0800

    git自动部署功能完善

--- src/system/default/_ctl/Gitdeploy.class.php
+++ src/system/default/_ctl/Gitdeploy.class.php
@@ -38,16 +38,18 @@ class Controller_Gitdeploy extends BaseController
             foreach ($deployArray as $k => $item) {
                 $resp   = $item[0];
                 $branch = empty($deployBranch) ? $item[1] : $deployBranch;
-
-                echo ($k + 1).": {$resp} {$branch}\n";
-
-                if (empty($item[2])) {
-                    $cmd = "git push {$resp} {$branch}";
-                } else {
-                    $this->_checkAndInitGitRepoForRemoteServer($resp, $branch, $item[2]);
-                    $cmd = "sshpass -p {$item[2]} git push {$resp} {$branch}";
+                if ($branch == '*') {
+                    exec('git config push.default matching');
+                    $branch = '';
                 }
-                exec($cmd);
+                echo ($k + 1).": {$resp} {$branch}\n";
+                if (empty($item[2])) {
+                    exec("git push {$resp} {$branch}");
+                } else {
+                    $this->_checkAndInitGitRepoForRemoteServer($resp, $item[2]);
+                    exec("sshpass -p {$item[2]} git push {$resp} {$branch}");
+                    $this->_checkAndChangeRemoteRepoToSpecifiedBranch($resp, $branch, $item[2]);
+                }
                 echo "\n";
             }
         }
@@ -56,30 +58,66 @@ class Controller_Gitdeploy extends BaseController
     /**
      * 检查目标服务器是否已经初始化。
      *
-     * @param $resp
-     * @param $branch
-     * @param $pass
+     * @param string $resp   版本库地址。
+     * @param string $pass   服务器SSH密码。
      */
-    private function _checkAndInitGitRepoForRemoteServer($resp, $branch, $pass)
+    private function _checkAndInitGitRepoForRemoteServer($resp, $pass)
     {
-        // 仓库格式形如： ssh://john@120.76.249.69//home/john/www/lge
-        if (preg_match("/ssh:\/\/(.+?)@([^:]+):{0,1}(\d*)\/(\/.+)/", $resp, $match)) {
-            $user   = $match[1];
-            $host   = $match[2];
-            $port   = empty($match[3]) ? 22 : $match[3];
-            $path   = rtrim($match[4], '/');
+        $parsed = $this->_parseRepository($resp);
+        if (!empty($parsed)) {
+            $user   = $parsed['user'];
+            $host   = $parsed['host'];
+            $port   = $parsed['port'];
+            $path   = $parsed['path'];
             $ssh    = new Lib_Network_Ssh($host, $port, $user, $pass);
             $result = $ssh->syncCmd("if [ -d \"{$path}/.git\" ]; then echo 1; else echo 0; fi");
             $result = trim($result);
             if ($result == "0") {
                 // 如果服务器的git目录不存在那么初始化目录
                 $ssh->syncCmd("mkdir -p \"{$path}\" && cd \"{$path}\" && git init && git config receive.denyCurrentBranch ignore");
-                $ssh->sendFile($this->_getGitPostReceiveHookFilePath(), $path.'/.git//hooks/post-receive', 0777);
-            } else {
-                // 否则更换分支为指定分支
-                $ssh->syncCmd("cd \"{$path}\" && git checkout {$branch} -f");
+                $ssh->sendFile($this->_getGitPostReceiveHookFilePath(), $path.'/.git/hooks/post-receive', 0777);
+                $ssh->disconnect();
             }
         }
+    }
+
+    /**
+     * 切换服务器上的分支为指定分支.
+     *
+     * @param string $resp   版本库地址。
+     * @param string $branch 分支名称。
+     * @param string $pass   服务器SSH密码。
+     */
+    private function _checkAndChangeRemoteRepoToSpecifiedBranch($resp, $branch, $pass)
+    {
+        $parsed = $this->_parseRepository($resp);
+        if (!empty($parsed)) {
+            $user   = $parsed['user'];
+            $host   = $parsed['host'];
+            $port   = $parsed['port'];
+            $path   = $parsed['path'];
+            $ssh    = new Lib_Network_Ssh($host, $port, $user, $pass);
+            $ssh->syncCmd("cd \"{$path}\" && git checkout {$branch} -f");
+            $ssh->disconnect();
+        }
+    }
+
+    /**
+     * 解析版本库，将ssh的版本库解析为用户账号、地址、端口、路径的形式。
+     * @param string $resp 版本库地址。
+     * @return array
+     */
+    private function _parseRepository($resp)
+    {
+        // 仓库格式形如： ssh://john@120.76.249.69//home/john/www/lge
+        $result = array();
+        if (preg_match("/ssh:\/\/(.+?)@([^:]+):{0,1}(\d*)\/(\/.+)/", $resp, $match)) {
+            $result['user'] = $match[1];
+            $result['host'] = $match[2];
+            $result['port'] = empty($match[3]) ? 22 : $match[3];
+            $result['path'] = rtrim($match[4], '/');
+        }
+        return $result;
     }
 
     /**
