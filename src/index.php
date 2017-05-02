cad8b5af28703dfc3b562b51959475667db0dd59
John <john@johng.cn>
2017-05-02 14:08:02 +0800

    处理phar被包含时的执行或者仅包含

--- src/index.php
+++ src/index.php
@@ -11,5 +11,10 @@ include(__DIR__.'/_cfg/const.inc.php');
 // 框架引入
 include(__DIR__.'/_frm/common.inc.php');
 
-// 框架初始化并执行
-\Lge\Core::initController();
+/*
+ * 执行流程初始化(控制器初始化)。
+ * 注意：当被其他项目包含引用时，不会执行控制器初始化操作，仅作包含。
+ */
+if (strpos(L_ROOT_PATH, 'phar://') !== false || file_exists(L_ROOT_PATH.'_frm')) {
+    \Lge\Core::initController();
+}
