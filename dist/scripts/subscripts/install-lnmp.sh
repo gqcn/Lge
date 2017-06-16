56b9bc7cad8668917c8987b19e1d89e8492a9a4a
John <john@johng.cn>
2017-06-16 14:50:37 +0800

    完善lge框架的终端shell管理功能

--- dist/scripts/subscripts/install-lnmp.sh
+++ dist/scripts/subscripts/install-lnmp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # 通过lge执行后续lnmp安装
-    echo -e "\033[32mInstalling lnmp using lge\033[0m"
+    echo "\033[32mInstalling lnmp using lge\033[0m"
     which lge > /dev/null 2>&1
     if [ $? = 0 ]; then
         lge lnmp
     else
-        echo -e "\033[31mlge not found, exit installation\033[0m"
+        echo "\033[31mlge not found, exit installation\033[0m"
         exit;
     fi
 
