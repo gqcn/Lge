2b1f693b3b1b5f4a143ab6cdf67bac71c3b3059d
John <john@johng.cn>
2017-06-18 19:55:37 +0800

    完善lge命令行脚本

--- dist/lge.sh
+++ dist/lge.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 用于安装lge命令到当前系统
+# 注意该脚本只安装lge命令(包括PHP-CLI)，并不会安装完整的PHP运行环境到当前系统
     # 获取当前执行脚本的绝对路径
     CURRENT_SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd)
 
@@ -8,7 +9,7 @@
        echo "\033[31mThis script must be running as root\033[0m"
        exit 1
     fi
-
+    
 # 先判断本地是否已经安装好了php cli
     which php > /dev/null 2>&1
     if [ $? = 0 ]; then
