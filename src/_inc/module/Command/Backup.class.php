75867ffd94a7c79dc0746ddee8a6d710387cdf87
John <john@johng.cn>
2017-08-28 15:18:53 +0800

    备份命令更新

--- src/_inc/module/Command/Backup.class.php
+++ src/_inc/module/Command/Backup.class.php
@@ -109,6 +109,8 @@ class Module_Command_Backup extends BaseModule
                                 // 将远程备份文件同步到备份中心
                                 $centerBackupFilePath = "{$centerBackupDir}/{$fileName}";
                                 $ssh->getFile($filePath, $centerBackupFilePath);
+                                var_dump(file_exists($centerBackupFilePath));
+                                var_dump(Lib_FileSys::getFileSize($centerBackupFilePath));
                                 if (!file_exists($centerBackupFilePath) || filesize($centerBackupFilePath) < 1024) {
                                     // 文件下载失败，那么远程执行命令主动同步到备份中心服务器上
                                     $shellCmd = "scp -P {$centerConfig['port']} {$filePath} {$centerConfig['user']}@{$centerConfig['host']}:{$centerBackupFilePath}";
